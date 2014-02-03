ffc7a7bb21ce831e868fba15f942e5bab95429b4
ardroz <darr.0725@gmail.com>
2014-02-02 20:29:59 -0600

    1-1

---
+++ arrayreplace.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int cambiarOcurrencias ( int *array, int toChange );
+
+int main() {
+
+  int arrayLength = 0,
+      i = 0,
+      toChange = 0,
+      changed = 0;
+
+  printf("Ingresa la longitud del arreglo: ");
+  scanf( "%d", &arrayLength );
+
+  int array [arrayLength];
+
+  for ( i = 0; i < arrayLength; i++ ) {
+    printf( "array[%d]: ", i );
+    scanf( "%d", &array[i] );
+  }
+
+  array[i] = 0;
+
+  printf("Ingresa el número a cambiar: ");
+  scanf( "%d", &toChange );
+
+  changed = cambiarOcurrencias( array, toChange );
+
+  printf("Se modificaron %d ocurrencias.\n", changed);
+
+  for ( i = 0; i < arrayLength; i++ ) {
+    printf( "array[%d]: %d\n", i, array[i] );
+  }
+  return 0;
+}
+
+int cambiarOcurrencias ( int *array, int toChange ) {
+  int i = 0,
+      changeTo = 0,
+      counter = 0;
+
+  printf("Cambiar por: ");
+  scanf( "%d", &changeTo );
+
+  while ( array[i] ) {
+    if ( *(array + i) == toChange ) {
+      *(array + i) = changeTo;
+      counter++;
+    }
+    i++;
+  }
+
+  return counter;
+}
